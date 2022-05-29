d897907777ab652d943df0d8158c45eadb61ddd1
N3rvy <manuelmanuel2003@gmail.com>
2022-05-29 09:08:54 +0200

    Formatted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import styles from './App.module.css';
-import Editor from './components/Editor';
-import init, {build_from_code} from "./assets/reverse-bin/portal_wasm_build"
+import styles from "./App.module.css";
+import Editor from "./components/Editor";
+import init, { build_from_code } from "./assets/reverse-bin/portal_wasm_build";
 
 const App = () => {
-    init()
+    init();
 
     const onChange = (s: string) => {
         try {
-            const module = build_from_code(s)
-            let memory: any = undefined
+            const module = build_from_code(s);
+            let memory: any = undefined;
 
             const imports = {
                 main: {
-                    "print": (strLoc: number) => {
-                        const lengthBuffer = memory.buffer.slice(strLoc, strLoc + 4)
-                        const len = new Int16Array(lengthBuffer)[0]
-                        const strBuffer = new Uint8Array(memory.buffer.slice(strLoc + 4, strLoc + 4 + len))
-                        const decoder = new TextDecoder()
-                        const str = decoder.decode(strBuffer)
+                    print: (strLoc: number) => {
+                        const lengthBuffer = memory.buffer.slice(
+                            strLoc,
+                            strLoc + 4
+                        );
+                        const len = new Int16Array(lengthBuffer)[0];
+                        const strBuffer = new Uint8Array(
+                            memory.buffer.slice(strLoc + 4, strLoc + 4 + len)
+                        );
+                        const decoder = new TextDecoder();
+                        const str = decoder.decode(strBuffer);
 
-                        console.log(str)
+                        console.log(str);
                     },
-                    "printI": (i: number) => console.log(i)
+                    printI: (i: number) => console.log(i),
+                },
+            };
+
+            WebAssembly.instantiate(module, imports).then((module) => {
+                console.log("Success!");
+
+                memory = module.instance.exports.mem;
+
+                const { main } = module.instance.exports;
+                if (main) {
+                    (main as any)();
                 }
-            }
-
-            WebAssembly.instantiate(module, imports)
-                .then((module) => {
-                    console.log("Success!")
-
-                    memory = module.instance.exports.mem
-                
-                    const {main} = module.instance.exports
-                    if (main) {
-                        (main as any)()
-                    }
-                })
-
-        } catch { }
-    }
+            });
+        } catch {}
+    };
 
     return (
         <div>
