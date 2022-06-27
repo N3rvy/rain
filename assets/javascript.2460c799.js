479780d5b2735c0fe843e335ddc36f62347dc0b1
N3rvy <manuelmanuel2003@gmail.com>
2022-06-27 19:30:17 +0200

    Updates

--- assets/javascript.2460c799.js
+++ assets/javascript.2460c799.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript.78145be4.js";import"./index.89f76871.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript.795128e2.js";import"./index.8391828a.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
